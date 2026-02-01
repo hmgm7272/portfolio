91362831289c2a57740211b0b13915c0c61b0bf1
hmgm7272 <yuart7272@gmail.com>
2026-02-01 16:56:49 +0900

    update portfolio

--- src/data/wips.ts
+++ src/data/wips.ts
@@ -12,14 +12,14 @@ export const wips: Entry[] = [
         id: "wip2",
         title: "アニソン風",
         description: "アニソン風のイントロを作りました 2026/01/06",
-        images: ["/images/IMG_6522.MOV"],
+        images: ["/images/IMG_6522.mp3"],
         body: "ストリングス練習期間"
     },
     {
         id: "wip3",
         title: "きゅるして耳コピ",
         description: "絶対無敵ラブソング耳コピしました 2026/01/03",
-        images: ["/images/IMG_6508.MOV", "/images/IMG_6509.MOV"],
+        images: ["/images/IMG_6508.mp3", "/images/IMG_6509.mp3"],
         body: "modo bass最高だ"
     },
 ];
